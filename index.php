617127230f6168b0468c2364cbb9cb7ca679b030
autotrof <upload.kurniawan@gmail.com>
2023-05-07 08:03:05 +0700

    manipulasi DOM tanpa JS

--- index.php
+++ index.php
@@ -1,43 +1,33 @@
 <html>
   <head>
-    <style>
-      table {
-        border-collapse: collapse;
-      }
-      table tr td {
-        border: 1px solid dimgray;
-        padding: 5px;
-      }
-    </style>
+    
   </head>
   <body>
-    <button onclick="doRequest()">Klik Ajax</button>
     <div>
-      <span id="nama"></span>
-      <table class="table-hasil"> 
-        
-      </table>
+      <div id="a">Ini Tulisan ber ID</div>
+      <button onclick="gantiTulisan1()">Ganti Tulisannya</button>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <div>
+      <div class="x">Ini div 1</div>
+      <div class="x">Ini div 2</div>
+      <button onclick="gantiTulisan2()">Ganti Tulisannya</button>
+    </div>
     <script>
-      function doRequest() {
-        $.ajax({
-          url: "/data.php",
-          success: function(res) {
-            $("#nama").text(res.nama)
-            $(".table-hasil").children('')
-            let tampilan_keahlian = ''
-            for (let keahlian of res.keahlian) {
-              tampilan_keahlian+=`
-                <tr>
-                  <td>${keahlian}</td>
-                </tr>
-              `
-            }
-            $(".table-hasil").append(tampilan_keahlian)
-          }
-        })
+      function gantiTulisan1() {
+        const el = document.getElementById('a')
+        // const el = document.querySelector('#a')
+        // const el = document.querySelector('[id="a"]')
+        el.innerText = "YES YES YES"
       }
+
+      function gantiTulisan2() {
+        let elements = document.querySelectorAll('.x')
+        for (let el of elements) {
+          el.innerText = "NONONO"
+        }
+      }
+      
     </script>
   </body>
 </html>
